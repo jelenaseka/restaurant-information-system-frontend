e04397b78dcfb811c17d655fe0ca602b375edf8b
jovansx <jovansimic995@gmail.com>
2021-11-04 15:01:51 +0100

    Finished login page view, as well as validating the input data.

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,16 +8,45 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   hide = true;
-  email = new FormControl('', [Validators.required, Validators.email]);
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$'),
+    ]),
+  });
+
   constructor() {}
 
   ngOnInit(): void {}
 
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
+  validateEmail() {
+    if (this.loginForm.get('email')?.hasError('required')) {
       return 'You must enter a value';
     }
+    return this.loginForm.get('email')?.hasError('email')
+      ? 'Not a valid email'
+      : '';
+  }
 
-    return this.email.hasError('email') ? 'Not a valid email' : '';
+  validatePassword() {
+    if (this.loginForm.get('password')?.hasError('required')) {
+      return 'You must enter a value';
+    }
+    if (this.loginForm.get('password')?.hasError('minlength')) {
+      return 'Minimum number of characters is 5';
+    }
+    return this.loginForm.get('password')?.hasError('pattern')
+      ? 'Has to contain lowercase, uppercase letters and numbers'
+      : '';
+  }
+
+  loginUser() {
+    console.log(this.loginForm.invalid);
+    if (this.loginForm.invalid) {
+      return;
+    }
+    console.warn(this.loginForm.value);
   }
 }
