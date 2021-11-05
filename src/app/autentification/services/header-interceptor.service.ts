2aabafe2aa0b34c2dd904899d2c3c7d1288fc786
Aleksandar Buljevic <abuljevic8@gmail.com>
2021-11-05 10:49:37 +0100

    Authorization header chanhed

--- src/app/autentification/services/header-interceptor.service.ts
+++ src/app/autentification/services/header-interceptor.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpEvent, HttpResponse, HttpRequest, HttpHandler } from '@angular/common/http';
+import { HttpInterceptor, HttpEvent, HttpRequest, HttpHandler } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class HeaderInterceptorService implements HttpInterceptor {
   intercept(httpRequest: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = localStorage.getItem('token');
-    let Bearer = "";
+    let Authorization = "";
     if(token)
-      Bearer = token;
-    return next.handle(httpRequest.clone({ setHeaders: { Bearer } }));
+      Authorization = "Bearer " + token;
+    return next.handle(httpRequest.clone({ setHeaders: { Authorization } }));
   }
 }
